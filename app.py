75b141c41641af9bdf7d6221a6368bf1dc52d8f9
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-06-03 21:56:00 -0300

    att 03-06

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+#from database import sentencas
+from routes.home import home_route
 import webview
 
 app = Flask(__name__)
 webview.create_window('Hello world', app)
+
+app.register_blueprint(home_route)
 
 if __name__ == "__main__":
     #app.run(debug=True)
--- database/db.py
+++ database/db.py
@@ -1,2 +1,1 @@
-# Database de memória
-
+sentencas = []
