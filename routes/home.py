b92efefd691f434573697dbf17c7a3620545298c
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-06-21 13:37:20 -0300

    att

--- routes/home.py
+++ routes/home.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, render_template, request
-from tokens.generate import gerar_sentenca
 from tokens import parser
-import time
 
 home_route = Blueprint('home', __name__)
 
@@ -9,12 +7,7 @@ home_route = Blueprint('home', __name__)
 def home():
     return render_template('index.html')
 
-@home_route.route('/gerar_sentencas', methods=['POST'])
-def gerar_sentencas():
-    sentencesInput = gerar_sentenca()
-    return render_template('index.html', sentencesInput=sentencesInput)
-
-@home_route.route('/gerar_parsing', methods=['POST'])
+@home_route.route('/', methods=['POST'])
 def gerar_parsing():
     automaton = parser.Automaton()  # Cria uma instância do autômato para cada requisição
     listsentencesInput = request.form.get('sentencesInput')
--- static/js/script.js
+++ static/js/script.js
@@ -3,16 +3,28 @@ function clearValue() {
     document.getElementById("table").innerHTML = '';
 }
 
+function generateSentence() {
+    clearValue();
+    const sentences = [
+        'badcc',
+        'dbcbdcd',
+        'dbcbdca',
+        'babadbccc',
+        'bababcdcaa',
+        'bababcdcbccc'
+    ];
+    const randomSentence = sentences[Math.floor(Math.random() * sentences.length)];
+    document.getElementById('sentencesInput').value = randomSentence;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    var form = document.getElementById('my-form');
+    let form = document.getElementById('my-form');
 
     form.addEventListener('submit', function(event) {
-        var stepByStepButton = document.getElementById('delayed-button');
+        let stepByStepButton = document.getElementById('stepbystep-button');
         
-        // Check if the step-by-step button was clicked
         if (event.submitter === stepByStepButton) {
-            // Add a flag to indicate step-by-step mode
-            var hiddenField = document.createElement("input");
+            let hiddenField = document.createElement("input");
             hiddenField.type = "hidden";
             hiddenField.name = "step_by_step";
             hiddenField.value = "true";
@@ -20,13 +32,12 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Check if we need to display rows step-by-step
     if (document.getElementById('step-by-step-flag')) {
-        var rows = document.querySelectorAll('.table-row');
+        let rows = document.querySelectorAll('.table-row');
         rows.forEach(function(row, index) {
-            row.style.display = 'none'; // Hide the row initially
+            row.style.display = 'none';
             setTimeout(function() {
-                row.style.display = 'table-row'; // Show the row after the interval
+                row.style.display = 'table-row';
             }, index * 1000);
         });
     }
