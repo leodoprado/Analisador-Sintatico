3a81029fbd54d743832bfb2128d2c1ac5c83a87a
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-06-21 08:17:33 -0300

    att 21-06

--- routes/home.py
+++ routes/home.py
@@ -19,5 +19,5 @@ def gerar_parsing():
     automaton = parser.Automaton()  # Cria uma instância do autômato para cada requisição
     listsentencesInput = request.form.get('sentencesInput')
     automaton.check_end(listsentencesInput)
-    return render_template('index.html', table=automaton.table, palavra=listsentencesInput)
-
+    step_by_step = request.form.get('step_by_step') == 'true'
+    return render_template('index.html', table=automaton.table, palavra=listsentencesInput, step_by_step=step_by_step)
--- static/js/script.js
+++ static/js/script.js
@@ -4,14 +4,30 @@ function clearValue() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var rows = document.querySelectorAll('.table-row');
-    rows.forEach(function(row, index) {
-        row.style.display = 'none'; // Oculta a linha inicialmente
-        setTimeout(function() {
-            row.style.display = 'table-row'; // Exibe a linha após o intervalo
-        }, index * 1000);
+    var form = document.getElementById('my-form');
+
+    form.addEventListener('submit', function(event) {
+        var stepByStepButton = document.getElementById('delayed-button');
+        
+        // Check if the step-by-step button was clicked
+        if (event.submitter === stepByStepButton) {
+            // Add a flag to indicate step-by-step mode
+            var hiddenField = document.createElement("input");
+            hiddenField.type = "hidden";
+            hiddenField.name = "step_by_step";
+            hiddenField.value = "true";
+            form.appendChild(hiddenField);
+        }
     });
+
+    // Check if we need to display rows step-by-step
+    if (document.getElementById('step-by-step-flag')) {
+        var rows = document.querySelectorAll('.table-row');
+        rows.forEach(function(row, index) {
+            row.style.display = 'none'; // Hide the row initially
+            setTimeout(function() {
+                row.style.display = 'table-row'; // Show the row after the interval
+            }, index * 1000);
+        });
+    }
 });
-
-
-
